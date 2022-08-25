b66b09dd8118ca25fcfa9e6a5a0125dfebc73c5c
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-08-25 08:05:42 +0200

    try 'parser' with cffi

--- python/cffi-build.py
+++ python/cffi-build.py
@@ -4,7 +4,6 @@ import header_processor as hp
 
 excluded_words = [ "" ]
 ffi = cffi.FFI()
-headerfile = "test.h"
 #with open(headerfile) as h_file:
 #    hellois_comment = False
 #    header_content = ""
@@ -22,6 +21,11 @@ headerfile = "test.h"
 #        header_content += line
 #    ffi.cdef(header_content)
 includes = hp.get_additional_compiler_flags(["glib-2.0", "julea", "julea-object", "julea-kv", "julea-db"])
+dirs = hp.get_include_dirs(includes)
+header_content = hp.read_header_file("/home/urz/diessner/julea/include/julea-kv.h", dirs)
+with open("out.h", "w") as file:
+    file.write(header_content)
+ffi.cdef(header_content)
 ffi.set_source(
         "julea_kv",
         '#include "include/julea-kv.h"',
