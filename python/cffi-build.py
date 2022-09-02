46ff850746d67dcafda7d3325d37fc08965fbfe4
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-09-02 15:52:37 +0200

    tweaking cffi args

--- python/cffi-build.py
+++ python/cffi-build.py
@@ -11,10 +11,15 @@ with open("header.h") as header:
             continue
         header_content += line
 includes = hp.get_additional_compiler_flags(["glib-2.0", "julea", "julea-object", "julea-kv", "julea-db"])
+include_dirs = hp.get_include_dirs(includes)
 ffi.set_source(
         "julea_kv",
-        '#include "include/julea-kv.h"',
+        """
+            #include "julea-kv.h"
+            #include "julea.h"
+        """,
         libraries=["julea-kv", "julea"],
+        include_dirs=include_dirs,
         library_dirs=["bld"],
         extra_compile_args=includes,
         extra_link_args=["-Wl,-rpath,."]
