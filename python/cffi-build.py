07687c58c8d1fb8746bd2419d6cdfd17616e66e3
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-09-02 15:28:09 +0200

    get include parameters

--- python/cffi-build.py
+++ python/cffi-build.py
@@ -1,5 +1,6 @@
 import cffi
 import os
+import header_processor as hp
 
 ffi = cffi.FFI()
 os.system("gcc -E -P -D'__attribute__(ARGS)=' -D'__restrict=' -D'__asm__(x)=' -D'__builtin_va_list=char*' -D'__extension__=' /home/user/julea/include/julea-kv.h $(pkg-config --cflags glib-2.0 julea julea-object julea-kv julea-db) -o header.h")
@@ -9,10 +10,7 @@ with open("header.h") as header:
         if line.startswith("#pragma GCC diagnostic"):
             continue
         header_content += line
-with open("header_new.h", "w") as file:
-    file.write(header_content)
-#with open("header_new.h") as file:
-#    ffi.cdef(file.read())
+includes = hp.get_additional_compiler_flags(["glib-2.0", "julea", "julea-object", "julea-kv", "julea-db"])
 ffi.set_source(
         "julea_kv",
         '#include "include/julea-kv.h"',
