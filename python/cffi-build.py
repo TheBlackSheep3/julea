d480ccbc72d666ae699a82c91acba70501194737
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-09-02 13:58:46 +0200

    read whole header, not line by line

--- python/cffi-build.py
+++ python/cffi-build.py
@@ -4,7 +4,7 @@ import os
 ffi = cffi.FFI()
 os.system("gcc -E -P /home/user/julea/include/julea-kv.h $(pkg-config --cflags glib-2.0 julea julea-object julea-kv julea-db) -o header.h")
 with open("header.h") as header:
-    header_content = header.readlines()
+    header_content = header.read()
 ffi.cdef(header_content)
 ffi.set_source(
         "julea_kv",
