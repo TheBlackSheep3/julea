04764aa184eb13a099b18f5099d6381709ad0c25
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-09-06 15:55:21 +0200

    progress on testing

--- python/cffi-build.py
+++ python/cffi-build.py
@@ -3,25 +3,51 @@ import os
 import header_processor as hp
 
 ffi = cffi.FFI()
-os.system("gcc -E -P -D'__attribute__(ARGS)=' -D'__restrict=' -D'__asm__(x)=' -D'__builtin_va_list=char*' -D'__extension__=' /home/user/julea/include/julea-kv.h $(pkg-config --cflags glib-2.0 julea julea-object julea-kv julea-db) -o header.h")
-header_content = ""
-with open("header.h") as header:
-    for line in header:
-        if line.startswith("#pragma GCC diagnostic"):
-            continue
-        header_content += line
-includes = hp.get_additional_compiler_flags(["glib-2.0", "julea", "julea-object", "julea-kv", "julea-db"])
-include_dirs = hp.get_include_dirs(includes)
-ffi.set_source(
-        "julea_kv",
-        """
-            #include "julea-kv.h"
-            #include "julea.h"
-        """,
-        libraries=["julea-kv", "julea"],
-        include_dirs=include_dirs,
-        library_dirs=["bld"],
-        extra_compile_args=includes,
-        extra_link_args=["-Wl,-rpath,."]
-        )
-ffi.compile(verbose=True)
+
+def prepare():
+    os.system("gcc -E -P -D'__inline=' -D'__inline__=' -D'__attribute__(ARGS)=' -D'__restrict=' -D'__asm__(x)=' -D'__builtin_va_list=char*' -D'__extension__=' /home/user/julea/include/julea-kv.h $(pkg-config --cflags glib-2.0 julea julea-object julea-kv julea-db) -o header.h")
+    header_content = ""
+    with open("header.h") as header:
+        for line in header:
+            if line.startswith("#pragma GCC diagnostic"):
+                continue
+            header_content += line
+    with open("header_strip.h", "w") as file:
+        file.write(header_content)
+
+def test():
+    with open("header_strip.h", "r") as file:
+        header_content = file.read()
+    includes = hp.get_additional_compiler_flags(["glib-2.0", "julea", "julea-object", "julea-kv", "julea-db"])
+    include_dirs = hp.get_include_dirs(includes)
+    ffi.cdef(header_content)
+    ffi.set_source(
+            "julea_kv",
+            """
+                #include "julea-kv.h"
+                #include "julea.h"
+            """,
+            libraries=["julea-kv", "julea"],
+            include_dirs=include_dirs,
+            library_dirs=["bld"],
+            extra_compile_args=includes,
+            extra_link_args=["-Wl,-rpath,."]
+            )
+    ffi.compile(verbose=True)
+
+def alt():
+    os.system("gcc -E -P -DJULEA_KV_COMPILATION -D'__inline=' -D'__inline__=' -D'__attribute__(ARGS)=' -D'__restrict=' -D'__asm__(x)=' -D'__builtin_va_list=char*' -D'__extension__=' /home/user/julea/include/julea-kv.h $(pkg-config --cflags glib-2.0 julea julea-object julea-kv julea-db) -o header.h")
+    os.system("gcc -E -P -DJULEA_KV_COMPILATION -D'__inline=' -D'__inline__=' -D'__attribute__(ARGS)=' -D'__restrict=' -D'__asm__(x)=' -D'__builtin_va_list=char*' -D'__extension__=' /home/user/julea/lib/kv/jkv.c $(pkg-config --cflags glib-2.0 julea julea-object julea-kv julea-db) -o jkv_pre.c")
+    os.system("gcc -E -P -DJULEA_KV_COMPILATION -D'__inline=' -D'__inline__=' -D'__attribute__(ARGS)=' -D'__restrict=' -D'__asm__(x)=' -D'__builtin_va_list=char*' -D'__extension__=' /home/user/julea/lib/kv/jkv-iterator.c $(pkg-config --cflags glib-2.0 julea julea-object julea-kv julea-db) -o jkv-iterator_pre.c")
+    os.system("gcc -E -P -DJULEA_KV_COMPILATION -D'__inline=' -D'__inline__=' -D'__attribute__(ARGS)=' -D'__restrict=' -D'__asm__(x)=' -D'__builtin_va_list=char*' -D'__extension__=' /home/user/julea/lib/kv/jkv-uri.c $(pkg-config --cflags glib-2.0 julea julea-object julea-kv julea-db) -o jkv-uri_pre.c")
+    includes = open("header.h").read()
+    source = open("jkv_pre.c").read()
+    source += open("jkv-iterator_pre.c").read()
+    source += open("jkv-uri_pre.c").read()
+
+    ffi.cdef(includes)
+    ffi.set_source("julea_kv", source, libraries=["julea-kv", "julea"], include_dirs=["/home/user/julea/include"])
+    ffi.compile(verbose=True)
+
+if __name__ == "__main__":
+    test()
