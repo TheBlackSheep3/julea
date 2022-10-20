fd5c2d3d78f7ebccc738a8aa4e202468414f4f25
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-10-21 00:18:51 +0200

    clean up python sub directory

--- python/header_processor.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-std::vector<std::string> get_include_dirs(std::vector<std::string> flags);
-std::vector<std::string> get_additional_compiler_flags(std::vector<std::string> libraries);
-std::string read_header_file(std::string file_path, std::vector<std::string> include_dirs);
