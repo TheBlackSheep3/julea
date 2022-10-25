7c345d7c3886b91a7e39a08c480886c0acdd4795
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-10-25 15:41:11 +0200

    change path for python module

--- python/example/write.py
+++ python/example/write.py
@@ -1,6 +1,7 @@
 import sys
+from os.path import dirname
 
-sys.path.insert(0, "/home/user/julea")
+sys.path.insert(0, "{currentdir}/../../bld/".format(currentdir=dirname(__file__)))
 
 from julea_kv import lib as libkv
 import cffi
