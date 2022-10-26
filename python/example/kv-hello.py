9ba34af25a8e84e7caa6413a59feb5b1d225129a
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-10-27 00:53:40 +0200

    use index function in example

--- python/example/kv-hello.py
+++ python/example/kv-hello.py
@@ -8,28 +8,41 @@ from julea_kv import lib as libkv
 import cffi
 ffi = cffi.FFI()
 
-batch = libkv.j_batch_new_for_template(libkv.J_SEMANTICS_TEMPLATE_DEFAULT)
-kv = libkv.j_kv_new("python".encode('utf-8'), "value".encode('utf-8'))
+def get_string(p_buffer, length):
+    res = b''
+    char = ffi.cast('char*', p_buffer[0])
+    for i in range(length):
+        res += char[i]
+    return res.decode()
 
-value = "Hello from Python!"
-libkv.j_kv_put(kv, ffi.new('char[]', value.encode('utf-8')), len(value)+1, ffi.NULL, batch)
+def ffi_encode(string):
+    p = ffi.new('char[]', string.encode('utf-8'))
+    return p
+
+batch = libkv.j_batch_new_for_template(libkv.J_SEMANTICS_TEMPLATE_DEFAULT)
+kv1 = libkv.j_kv_new("python".encode('utf-8'), "value".encode('utf-8'))
+kv2 = libkv.j_kv_new_for_index(0, "my".encode('utf-8'), "other".encode('utf-8'))
+
+value1 = "Hello from Python!"
+value2 = "test"
+char_p1 = ffi_encode(value1)
+char_p2 = ffi_encode(value2)
+libkv.j_kv_put(kv1, char_p1, len(value1)+1, ffi.NULL, batch)
+libkv.j_kv_put(kv2, char_p2, len(value2)+1, ffi.NULL, batch)
 
 if (libkv.j_batch_execute(batch)):
-    length = ffi.new('unsigned int*')
-    p = ffi.new('void**')
-    libkv.j_kv_get(kv, p, length, batch)
+    length1 = ffi.new('unsigned int*')
+    p1 = ffi.new('void**')
+    length2 = ffi.new('unsigned int*')
+    p2 = ffi.new('void**')
+    libkv.j_kv_get(kv1, p1, length1, batch)
+    libkv.j_kv_get(kv2, p2, length2, batch)
     if (libkv.j_batch_execute(batch)):
-        char = ffi.cast('char*', p[0])
-        string = ""
-        i = 0
-        byte = char[i]
-        while byte != b'\x00':
-            string += byte.decode()
-            i += 1
-            byte = char[i]
-        print("Python says KV contains: '{value}' ({length} bytes)".format(value=string, length=length[0]))
+        print("Python says KV contains: '{value}' ({length} bytes)".format(value=get_string(p1, length1[0]), length=length1[0]))
+        print("Python says KV contains: '{value}' ({length} bytes)".format(value=get_string(p2, length2[0]), length=length2[0]))
         # clean up
-        libkv.j_kv_delete(kv, batch)
+        libkv.j_kv_delete(kv1, batch)
+        libkv.j_kv_delete(kv2, batch)
         libkv.j_batch_execute(batch)
     sys.exit(0)
 
