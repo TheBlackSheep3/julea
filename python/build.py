f57868494299b310908f02f942b8ee29f4e166bb
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-11-07 19:57:08 +0100

    added julea-item in build process

--- python/build.py
+++ python/build.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     build("julea_object", ["julea", "julea-object"])
     build("julea_kv", ["julea", "julea-kv"])
     build("julea_db", ["julea", "julea-db"])
+    build("julea_item", ["julea", "julea-item"])
