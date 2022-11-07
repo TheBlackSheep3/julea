ec49a885ebc246826693dc28d50f1c9109842bf3
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-11-07 19:44:46 +0100

    added julea-db in build process

--- python/build.py
+++ python/build.py
@@ -4,3 +4,4 @@ from build_helper import build
 if __name__ == "__main__":
     build("julea_object", ["julea", "julea-object"])
     build("julea_kv", ["julea", "julea-kv"])
+    build("julea_db", ["julea", "julea-db"])
