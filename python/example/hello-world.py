1f3435618ad709e37ac69c590973fb22725130d4
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-11-13 18:53:23 +0100

    added hello world python

--- python/example/hello-world.py
+++ python/example/hello-world.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
         result = JBatchResult()
         hello = encode("hello")
         world = encode("world")
+        nbytes_ptr = ffi.new("unsigned long*")
         _object = JObject.j_object_new(hello, world)
         kv = JKV.j_kv_new(hello, world)
         schema = JDB.j_db_schema_new(hello, world, NULL)
@@ -18,17 +19,18 @@ if __name__ == "__main__":
         JDB.j_db_entry_set_field(entry, hello, encode(value), len(value) + 1, NULL)
         with JBatch(result) as batch:
             JObject.j_object_create(_object, batch)
-            JObject.j_object_write(_object, encode(value), len(value) + 1, 0, ??, batch)
+            JObject.j_object_write(_object, encode(value), len(value) + 1, 0, nbytes_ptr, batch)
             JKV.j_kv_put(kv, encode(value), len(value) + 1, NULL, batch)
             JDB.j_db_schema_create(schema, batch, NULL)
             JDB.j_db_entry_insert(entry, batch, NULL)
 
         if result.IsSuccess:
             result = JBatchResult()
+            buffer = ffi.new("gchar[]", 128)
             with JBatch(result) as batch:
-                JObject.j_object_read(_object, ?buffer?, 128, 0, ??, batch)
+                JObject.j_object_read(_object, buffer, 128, 0, nbytes_ptr, batch)
             if result.IsSuccess:
-                print("Object contains: '{value}' ({length} bytes)".format(value=read_from_buffer(??), ??))
+                print("Object contains: '{value}' ({length} bytes)".format(value=read_from_buffer(buffer), length=nbytes_ptr[0]))
 
             result = JBatchResult()
             with JBatch(result) as batch:
@@ -45,8 +47,10 @@ if __name__ == "__main__":
                 
                 while JDB.j_db_iterator_next(iterator, NULL):
                     _type = ffi.new("JDBType *")
-                    JDB.j_db_iterator_get_field(iterator, hello, _type[0], ??, ??, NULL)
-                    print("DB contains: '{value}' ({length} bytes".format(value=read_from_buffer(??), ??))
+                    db_field_ptr = ffi.new("gchar**")
+                    db_length_ptr = ffi.new("unsigned long*")
+                    JDB.j_db_iterator_get_field(iterator, hello, _type[0], db_field_ptr, db_length_ptr, NULL)
+                    print("DB contains: '{value}' ({length} bytes".format(value=read_from_buffer(db_field_ptr[0]), length=db_length_ptr[0]))
 
             finally:
                 JDB.j_db_selector_unref(selector)
