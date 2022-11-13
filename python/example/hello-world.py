9553ae342f6d37286039508b353284247539e15b
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-11-13 21:06:43 +0100

    fix key value and object

--- python/example/hello-world.py
+++ python/example/hello-world.py
@@ -1,8 +1,9 @@
-from julea import JBatchResult, JBatch, ffi, lib, encode, read_from_buffer, get_void_ptr_ptr, get_uint_ptr
+from julea import JBatchResult, JBatch, ffi, lib, encode, read_from_buffer
 
 if __name__ == "__main__":
     try:
-        value = "Hello World!"
+        value_obj = "Hello Object!"
+        value_kv = "Hello Key-Value!"
 
         result = JBatchResult()
         hello = encode("hello")
@@ -14,29 +15,34 @@ if __name__ == "__main__":
         lib.j_db_schema_add_field(schema, hello, lib.J_DB_TYPE_STRING, ffi.NULL)
         entry = lib.j_db_entry_new(schema, ffi.NULL)
 
-        lib.j_db_entry_set_field(entry, hello, encode(value), len(value) + 1, ffi.NULL)
+        #lib.j_db_entry_set_field(entry, hello, encode(value), len(value) + 1, ffi.NULL)
+
         with JBatch(result) as batch:
-            #lib.j_object_create(_object, batch)
-            #lib.j_object_write(_object, encode(value), len(value) + 1, 0, nbytes_ptr, batch)
-            lib.j_kv_put(kv, encode(value), len(value) + 1, ffi.NULL, batch)
-            #lib.j_db_schema_create(schema, batch, ffi.NULL)
-            #lib.j_db_entry_insert(entry, batch, ffi.NULL)
+            lib.j_kv_delete(kv, batch)
+            lib.j_object_delete(_object, batch)
+
+        with JBatch(result) as batch:
+            lib.j_object_create(_object, batch)
+            lib.j_object_write(_object, encode(value_obj), len(value_obj) + 1, 0, nbytes_ptr, batch)
+            lib.j_kv_put(kv, encode(value_kv), len(value_kv) + 1, ffi.NULL, batch)
+#            lib.j_db_schema_create(schema, batch, ffi.NULL)
+#            lib.j_db_entry_insert(entry, batch, ffi.NULL)
 
         if result.IsSuccess:
-#            result = JBatchResult()
-#            buffer = ffi.new("gchar[]", 128)
-#            with JBatch(result) as batch:
-#                lib.j_object_read(_object, buffer, 128, 0, nbytes_ptr, batch)
-#            if result.IsSuccess:
-#                print("Object contains: '{value}' ({length} bytes)".format(value=read_from_buffer(buffer), length=nbytes_ptr[0]))
+            result = JBatchResult()
+            buffer = ffi.new("gchar[]", 128)
+            with JBatch(result) as batch:
+                lib.j_object_read(_object, buffer, 128, 0, nbytes_ptr, batch)
+            if result.IsSuccess:
+                print("Object contains: '{value}' ({length} bytes)".format(value=read_from_buffer(buffer), length=nbytes_ptr[0]))
 
             result = JBatchResult()
             with JBatch(result) as batch:
-                ptr = get_void_ptr_ptr()
-                length = get_uint_ptr()
-                lib.j_kv_get(kv, ptr, length, batch)
+                buffer_ptr = ffi.new("void**")
+                length = ffi.new("unsigned int *")
+                lib.j_kv_get(kv, buffer_ptr, length, batch)
             if result.IsSuccess:
-                print("KV contains: '{value}' ({length} bytes)".format(value=read_from_buffer(ptr[0]), length=length[0]))
+                print("KV contains: '{value}' ({length} bytes)".format(value=read_from_buffer(buffer_ptr[0]), length=length[0]))
 
 #            try:
 #                selector = lib.j_db_selector_new(schema, lib.J_DB_SELECTOR_MODE_AND, ffi.NULL)
@@ -56,6 +62,6 @@ if __name__ == "__main__":
 
     finally:
         lib.j_kv_unref(kv)
-#        lib.j_object_unref(_object)
+        lib.j_object_unref(_object)
 #        lib.j_db_schema_unref(schema)
 #        lib.j_db_entry_unref(entry)
