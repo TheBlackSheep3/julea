f7d428e4405f143fac7f0a8a26b8b4d7c82c297d
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-11-15 15:33:38 +0100

    call benchmarks with parameters

--- benchmark/python/benchmark.py
+++ benchmark/python/benchmark.py
@@ -1,16 +1,22 @@
+from kv import benchmark_kv
+from object import benchmark_object()
+
 if __name__ == "__main__":
+    runs = []
+    iterations = 1000
+
     # KV Client
-    benchmark_kv()
+    benchmark_kv(runs, iterations)
 
     # Object Client
-    benchmark_distributed_object()
-    benchmark_object()
+    benchmark_distributed_object(runs, iterations)
+    benchmark_object(runs, iterations)
 
     # DB Client
-    benchmark_db_entry()
-    benchmark_db_iterator()
-    benchmark_db_schema()
+    benchmark_db_entry(runs, iterations)
+    benchmark_db_iterator(runs, iterations)
+    benchmark_db_schema(runs, iterations)
 
     # Item Client
-    benchmark_collection()
-    benchmark_item()
+    benchmark_collection(runs, iterations)
+    benchmark_item(runs, iterations)
