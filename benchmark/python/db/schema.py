ca1daa429f514766c997b10462ed742a6225efc7
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-12-26 20:59:04 +0100

    wrote frame for db schema benchmark in python

--- benchmark/python/db/schema.py
+++ benchmark/python/db/schema.py
@@ -1,3 +1,28 @@
+from benchmarkrun import BenchmarkRun, append_to_benchmark_list_and_run
+from julea import lib, encode, ffi
+
 def benchmark_db_schema(benchmarkrun_list, iterations):
-    print("benchmark_db_schema called")
-    # TODO: implement db schema benchmark (db/schema.c)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/schema/create", iterations), benchmark_db_schema_create)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/schema/create-batch", iterations), benchmark_db_schema_create_batch)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/schema/delete", iterations), benchmark_db_schema_delete)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/schema/delete-batch", iterations), benchmark_db_schema_delete_batch)
+
+def benchmark_db_schema_create(run):
+    _benchmark_db_schema_create(run, False)
+
+def benchmark_db_schema_create_batch(run):
+    _benchmark_db_schema_create(run, True)
+
+def _benchmark_db_schema_create(run, use_batch):
+    # TODO: implement _benchmark_db_schema_create
+    return
+
+def benchmark_db_schema_delete(run):
+    _benchmark_db_schema_delete(run, False)
+
+def benchmark_db_schema_delete_batch(run):
+    _benchmark_db_schema_delete(run, True)
+
+def _benchmark_db_schema_delete(run, use_batch):
+    # TODO: implement _benchmark_db_schema_delete
+    return
