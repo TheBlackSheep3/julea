959beebf09b40d740376c39150f295c2fe148975
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2022-12-26 20:33:06 +0100

    wrote frame for db entry benchmark in python

--- benchmark/python/db/entry.py
+++ benchmark/python/db/entry.py
@@ -1,3 +1,148 @@
+from benchmarkrun import BenchmarkRun, append_to_benchmark_list_and_run
+from julea import lib, encode, ffi
+
 def benchmark_db_entry(benchmarkrun_list, iterations):
-    print("benchmark_db_entry called")
-    # TODO: implement db entry benchmark (db/entry.c)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/insert", iterations), benchmark_db_insert)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/insert-batch", iterations), benchmark_db_insert_batch)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/insert-index-single", iterations), benchmark_db_insert_index_single)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/insert-batch-index-single", iterations), benchmark_db_insert_batch_index_single)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/insert-index-all", iterations), benchmark_db_insert_index_all)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/insert-batch-index-all", iterations), benchmark_db_insert_batch_index_all)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/insert-index-mixed", iterations), benchmark_db_insert_index_mixed)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/insert-batch-index-mixed", iterations), benchmark_db_insert_batch_index_mixed)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/delete", iterations), benchmark_db_delete)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/delete-batch", iterations), benchmark_db_delete_batch)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/delete-index-single", iterations), benchmark_db_delete_index_single)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/delete-batch-index-single", iterations), benchmark_db_delete_batch_index_single)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/delete-index-all", iterations), benchmark_db_delete_index_all)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/delete-batch-index-all", iterations), benchmark_db_delete_batch_index_all)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/delete-index-mixed", iterations), benchmark_db_delete_index_mixed)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/delete-batch-index-mixed", iterations), benchmark_db_delete_batch_index_mixed)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/update", iterations), benchmark_db_update)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/update-batch", iterations), benchmark_db_update_batch)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/update-index-single", iterations), benchmark_db_update_index_single)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/update-batch-index-single", iterations), benchmark_db_update_batch_index_single)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/update-index-all", iterations), benchmark_db_update_index_all)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/update-batch-index-all", iterations), benchmark_db_update_batch_index_all)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/update-index-mixed", iterations), benchmark_db_update_index_mixed)
+	append_to_benchmark_list_and_run(benchmarkrun_list, BenchmarkRun("/db/entry/update-batch-index-mixed", iterations), benchmark_db_update_batch_index_mixed)
+
+def benchmark_db_insert(run):
+    _benchmark_db_insert(run, False)
+
+def benchmark_db_insert_batch(run):
+    _benchmark_db_insert(run, True)
+
+def _benchmark_db_insert(run, use_batch):
+    # TODO: implement _benchmark_db_insert
+    return
+
+def benchmark_db_insert_index_single(run):
+    _benchmark_db_insert_index_single(run, False)
+
+def benchmark_db_insert_batch_index_single(run):
+    _benchmark_db_insert_index_single(run, True)
+
+def _benchmark_db_insert_index_single(run, use_batch):
+    # TODO: implement _benchmark_db_insert_index_single
+    return
+
+def benchmark_db_insert_index_all(run):
+    _benchmark_db_insert_index_all(run, False)
+
+def benchmark_db_insert_batch_index_all(run):
+    _benchmark_db_insert_index_all(run, True)
+
+def _benchmark_db_insert_index_all(run, use_batch):
+    # TODO: implement _benchmark_db_insert_index_all
+    return
+
+def benchmark_db_insert_index_mixed(run):
+    _benchmark_db_insert_index_mixed(run, False)
+
+def benchmark_db_insert_batch_index_mixed(run):
+    _benchmark_db_insert_index_mixed(run, True)
+
+def _benchmark_db_insert_index_mixed(run, use_batch):
+    # TODO: implement _benchmark_db_insert_index_mixed
+    return
+
+def benchmark_db_delete(run):
+    _benchmark_db_delete(run, False)
+
+def benchmark_db_delete_batch(run):
+    _benchmark_db_delete(run, True)
+
+def _benchmark_db_delete(run, use_batch):
+    # TODO: implement _benchmark_db_delete
+    return
+
+def benchmark_db_delete_index_single(run):
+    _benchmark_db_delete_index_single(run, False)
+
+def benchmark_db_delete_batch_index_single(run):
+    _benchmark_db_delete_index_single(run, True)
+
+def _benchmark_db_delete_index_single(run, use_batch):
+    # TODO: implement _benchmark_db_delete_index_single
+    return
+
+def benchmark_db_delete_index_all(run):
+    _benchmark_db_delete_index_all(run, False)
+
+def benchmark_db_delete_batch_index_all(run):
+    _benchmark_db_delete_index_all(run, True)
+
+def _benchmark_db_delete_index_all(run, use_batch):
+    # TODO: implement _benchmark_db_delete_index_all
+    return
+
+def benchmark_db_delete_index_mixed(run):
+    _benchmark_db_delete_index_mixed(run, False)
+
+def benchmark_db_delete_batch_index_mixed(run):
+    _benchmark_db_delete_index_mixed(run, True)
+
+def _benchmark_db_delete_index_mixed(run, use_batch):
+    # TODO: implement _benchmark_db_delete_index_mixed
+    return
+
+def benchmark_db_update(run):
+    _benchmark_db_update(run, False)
+
+def benchmark_db_update_batch(run):
+    _benchmark_db_update(run, True)
+
+def _benchmark_db_update(run, use_batch):
+    # TODO: implement _benchmark_db_update
+    return
+
+def benchmark_db_update_index_single(run):
+    _benchmark_db_update_index_single(run, False)
+
+def benchmark_db_update_batch_index_single(run):
+    _benchmark_db_update_index_single(run, True)
+
+def _benchmark_db_update_index_single(run, use_batch):
+    # TODO: implement _benchmark_db_update_index_single
+    return
+
+def benchmark_db_update_index_all(run):
+    _benchmark_db_update_index_all(run, False)
+
+def benchmark_db_update_batch_index_all(run):
+    _benchmark_db_update_index_all(run, True)
+
+def _benchmark_db_update_index_all(run, use_batch):
+    # TODO: implement _benchmark_db_update_index_all
+    return
+
+def benchmark_db_update_index_mixed(run):
+    _benchmark_db_update_index_mixed(run, False)
+
+def benchmark_db_update_batch_index_mixed(run):
+    _benchmark_db_update_index_mixed(run, True)
+
+def _benchmark_db_update_index_mixed(run, use_batch):
+    # TODO: implement _benchmark_db_update_index_mixed
+    return
