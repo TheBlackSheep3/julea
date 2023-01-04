aafe47013eb21a1ca02dbf5bee6e2290d867fd7c
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2023-01-04 10:49:39 +0100

    call all benchmark functions in the python root benchmark script

--- benchmark/python/benchmark.py
+++ benchmark/python/benchmark.py
@@ -1,31 +1,30 @@
-#from kv.kv import benchmark_kv
-#from object.object import benchmark_object
-#from object.distributed_object import benchmark_distributed_object
-#from db.entry import benchmark_db_entry
+from kv.kv import benchmark_kv
+from object.object import benchmark_object
+from object.distributed_object import benchmark_distributed_object
+from db.entry import benchmark_db_entry
 from db.iterator import benchmark_db_iterator
-#from db.schema import benchmark_db_schema
-#from item.collection import benchmark_collection
-#from item.item import benchmark_item
+from db.schema import benchmark_db_schema
+from item.collection import benchmark_collection
+from item.item import benchmark_item
 
 if __name__ == "__main__":
     runs = []
     iterations = 1000
 
     # KV Client
-    #benchmark_kv(runs, iterations)
+    benchmark_kv(runs, iterations)
 
     # Object Client
-    #benchmark_distributed_object(runs, iterations)
-    #benchmark_object(runs, iterations)
+    benchmark_distributed_object(runs, iterations)
+    benchmark_object(runs, iterations)
 
     # DB Client
-    # FIXME: comment in other benchmark calls
-    #benchmark_db_entry(runs, iterations)
+    benchmark_db_entry(runs, iterations)
     benchmark_db_iterator(runs, iterations)
-    #benchmark_db_schema(runs, iterations)
+    benchmark_db_schema(runs, iterations)
 
     # Item Client
-    #benchmark_collection(runs, iterations)
-    #benchmark_item(runs, iterations)
-    #for run in runs:
-        #print(f"run {run.name} took {run.get_runtime_s()} s with {run.get_runtime_ms()/run.operations} ms per operation")
+    benchmark_collection(runs, iterations)
+    benchmark_item(runs, iterations)
+    for run in runs:
+        print(f"run {run.name} took {run.get_runtime_s()} s with {run.get_runtime_ms()/run.operations} ms per operation")
