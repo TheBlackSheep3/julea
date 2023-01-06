45bfa02b53095c6c8e690325f039a5d0a4fa1473
TheBlackSheep3 <40034835+TheBlackSheep3@users.noreply.github.com>
2023-01-06 15:11:48 +0100

    python benchmark made output machine readable

--- benchmark/python/benchmark.py
+++ benchmark/python/benchmark.py
@@ -6,25 +6,27 @@ from db.iterator import benchmark_db_iterator
 from db.schema import benchmark_db_schema
 from item.collection import benchmark_collection
 from item.item import benchmark_item
-from benchmarkrun import print_result_table_header
+from benchmarkrun import print_header
+from sys import argv
 
 if __name__ == "__main__":
     runs = []
     iterations = 1000
-    print_result_table_header()
+    machine_readable = ("-m" in argv)
+    print_header(machine_readable)
 
     # KV Client
-    benchmark_kv(runs, iterations)
+    benchmark_kv(runs, iterations, machine_readable)
 
     # Object Client
-    benchmark_distributed_object(runs, iterations)
-    benchmark_object(runs, iterations)
+    benchmark_distributed_object(runs, iterations, machine_readable)
+    benchmark_object(runs, iterations, machine_readable)
 
     # DB Client
-    benchmark_db_entry(runs, iterations)
-    benchmark_db_iterator(runs, iterations)
-    benchmark_db_schema(runs, iterations)
+    benchmark_db_entry(runs, iterations, machine_readable)
+    benchmark_db_iterator(runs, iterations, machine_readable)
+    benchmark_db_schema(runs, iterations, machine_readable)
 
     # Item Client
-    benchmark_collection(runs, iterations)
-    benchmark_item(runs, iterations)
+    benchmark_collection(runs, iterations, machine_readable)
+    benchmark_item(runs, iterations, machine_readable)
--- benchmark/python/benchmarkrun.py
+++ benchmark/python/benchmarkrun.py
@@ -1,13 +1,15 @@
 from time import perf_counter_ns
 
 class BenchmarkRun:
-    def __init__(self, name, iterations):
+    def __init__(self, name, iterations, machine_readable):
         self.name = name
         self.iterations = iterations
         self.timer_started = False
         self.start = None
         self.stop = None
         self.operations = iterations
+        self.bytes = None
+        self.machine_readable = machine_readable
 
     def start_timer(self):
         self.timer_started = True
@@ -16,10 +18,7 @@ class BenchmarkRun:
     def stop_timer(self):
         self.timer_started = False
         self.stop = perf_counter_ns()
-        name_col = self.name.ljust(60," ")
-        runtime_col = f"{self.get_runtime_s():.3f}".rjust(8," ") + " seconds"
-        operations_col = f"{int(self.operations/self.get_runtime_s())}/s".rjust(12," ")
-        print(f"{name_col} | {runtime_col} | {operations_col}")
+        self.print_result(self.machine_readable)
 
     def get_runtime_ms(self):
         val = self.get_runtime_ns()
@@ -35,6 +34,16 @@ class BenchmarkRun:
         else:
             return self.stop - self.start
 
+    def print_result(self, machine_readable):
+        if machine_readable:
+            print(f"{self.name},{self.get_runtime_s()},{self.operations},{'-' if self.bytes == None else self.bytes}")
+        else:
+            name_col = self.name.ljust(60," ")
+            runtime_col = f"{self.get_runtime_s():.3f}".rjust(8," ") + " seconds"
+            operations_col = f"{int(self.operations/self.get_runtime_s())}/s".rjust(12," ")
+            print(f"{name_col} | {runtime_col} | {operations_col}")
+
+
 def append_to_benchmark_list_and_run(_list, run, func):
     _list.append(run)
     func(run)
@@ -45,3 +54,12 @@ def print_result_table_header():
     operations_col = f"Operations/s".rjust(12," ")
     header = f"{name_col} | {runtime_col} | {operations_col}"
     print(header+"\n"+len(header)*"-")
+
+def print_machine_readable_header():
+    print("name,elapsed,operations,bytes")
+
+def print_header(machine_readable):
+    if machine_readable:
+        print_machine_readable_header()
+    else:
+        print_result_table_header()
